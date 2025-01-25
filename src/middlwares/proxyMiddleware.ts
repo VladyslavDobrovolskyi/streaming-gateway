65bc1e674bf414b80761557b6272290e78f21523
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-25 23:24:45 +0200

    Update authProxy middleware to change route from '/api/auth/' to '/auth' and enable path rewriting

--- src/middlwares/proxyMiddleware.ts
+++ src/middlwares/proxyMiddleware.ts
@@ -2,8 +2,11 @@ import { createProxyMiddleware } from 'http-proxy-middleware'
 
 export const authProxy = createProxyMiddleware({
 	router: {
-		'/api/auth/': 'http://streaming-auth:5555',
+		'/auth': 'http://streaming-auth:5555',
 	},
 	target: 'http://streaming-auth:5555', // Замените на адрес вашего Spring аутентификационного сервера
-	changeOrigin: false,
+	changeOrigin: true,
+	pathRewrite: {
+		'^/auth': '', // Удаляет /auth из пути запроса
+	},
 })
