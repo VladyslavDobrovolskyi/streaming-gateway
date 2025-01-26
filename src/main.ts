ecb98e4c970fa57cc4c16cff050f675a8029cddd
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-26 12:09:22 +0200

    Comment out authProxy middleware in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const port = 8888
 
 app.use(express.json())
 
-app.use('/auth', authProxy)
+// app.use('/auth', authProxy)
 app.use('/users', userRoutes)
 app.use('/room_reservations', roomReservationsRoutes)
 
