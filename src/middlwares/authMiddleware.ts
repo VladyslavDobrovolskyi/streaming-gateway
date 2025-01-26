75004567ba68a1aa4fb91178dcb0d660eb4e6862
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-26 15:55:50 +0200

    Retrieve token from Authorization header instead of cookies in authMiddleware

--- src/middlwares/authMiddleware.ts
+++ src/middlwares/authMiddleware.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 const secretKey = 'SM!@#^SECR*@#&18ET_-(JK_eY' // Use a secure key in production
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-	const token = req.cookies?.token
+	const token = req.header('Authorization')?.replace('Bearer ', '')
 
 	console.log('token', token)
 	if (!token) {
