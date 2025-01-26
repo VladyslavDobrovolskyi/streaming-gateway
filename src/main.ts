75c0aa0dd557eed9fdbe6937df862352dedb94dc
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-26 11:53:25 +0200

    Enable authProxy middleware for authentication requests in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 import userRoutes from './routes/userRoutes'
 import roomReservationsRoutes from './routes/roomRoutes'
-// import { authProxy } from './middlwares/proxyMiddleware'
+import { authProxy } from './middlwares/proxyMiddleware'
 
 const app = express()
 const port = 8888
 
 app.use(express.json())
 
-// app.use('/auth', authProxy)
+app.use('/auth', authProxy)
 app.use('/users', userRoutes)
 app.use('/room_reservations', roomReservationsRoutes)
 
