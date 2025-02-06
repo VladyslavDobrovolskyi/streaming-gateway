d9acef5751b29fd5c4ae74f12a565c995f0084f1
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-06 20:45:29 +0200

    Update emoji route to use async/await for retrieving emoji avatar

--- src/routes/emojiRoute.ts
+++ src/routes/emojiRoute.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from 'express'
 import getEmojiAvatar from '../utils/getEmojiAvatar'
 const router = express.Router()
 
-router.get('/emoji', (_req: Request, res: Response) => {
-	const url = getEmojiAvatar()
+router.get('/emoji', async (_req: Request, res: Response) => {
+	const url = await getEmojiAvatar()
 	res.json({ url })
 })
 
