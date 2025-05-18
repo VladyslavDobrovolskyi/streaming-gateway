fa8a3325ceae3fac4915239cfd880937329ad3fd
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-18 15:46:59 +0300

    Add CORS support to the application and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import express from 'express'
 import userRoutes from './routes/userRoutes'
 import roomReservationsRoutes from './routes/roomRoutes'
 import emojiRoute from './routes/emojiRoute'
+import cors from 'cors'
 // import { authProxy } from './middlwares/proxyMiddleware'
 
 const app = express()
 const port = 20888
-
+app.use(
+	cors({
+		origin: 'https://watchtogether.fun',
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	})
+)
 app.use(express.json())
 
 // app.use('/auth', authProxy)
